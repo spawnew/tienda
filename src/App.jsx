97d2609fa0cec674d28a9f70dbea6d5ef7168973
Lucas Sorzio <sorziolucas4@gmail.com>
2025-10-14 14:34:57 -0300

    creacion de navbar y instalacion de browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 
 import './App.css'
-
+import Contacto from './Componentes/Contacto/Contacto.jsx'
+import Footer from './Componentes/Footer/Footer.jsx'
+import Navbar from './Componentes/Navbar/Navbar.jsx'
+import Productos from './Componentes/Productos/Productos.jsx'
+import {Routes,Route, BrowserRouter} from 'react-router-dom' 
 function App() {
  
 
   return (
     <>
-      
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Productos/>}/>
+        <Route path='/contacto' element={<Contacto/>}/>
+     
+      </Routes>
+     
+      <Footer />
     </>
 )}
 export default App
