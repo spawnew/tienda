ba06eaa0b9f9910d42300a75807bfa3b984d5013
Lucas Sorzio <sorziolucas4@gmail.com>
2025-10-22 21:53:50 -0300

    c

--- src/Componentes/Form/Form.jsx
+++ src/Componentes/Form/Form.jsx
@@ -24,7 +24,7 @@ const Form = ({obtener}) => {
       <div>
           
 <form onSubmit={handleSubmit}>
-                <input className='p-2 bg-amber-50 rounded-2xl text-black' type="text" onChange={handleChange} name="producto" value={form.producto} placeholder='Buscar un articulo'>
+                <input className='p-3 w-150 md:w-100  bg-amber-50 rounded-2xl text-black' type="text" onChange={handleChange} name="producto" value={form.producto} placeholder='Buscar un articulo'>
                 </input>
                 <Link to="/producto"> 
                 <button className='login-btn' type='submit'> <FaSearch /></button>
--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const { contar } = useContext(ContextCart)
  
   return (
-    <nav  className=" flex flex-col bg-blue-900 flex-wrap md:text-2sm hover:scale-105 transition duration-300   text-white p-1  shadow-lg  md:flex-row md:justify-around md:items-center ">
+    <nav  className=" flex flex-col bg-blue-950 flex-wrap md:text-2sm hover:scale-105 transition duration-300   text-white p-1  shadow-lg  md:flex-row md:justify-around md:items-center ">
 
       <div className='titulo flex flex-col md:flex-row md:items-center gap-4 '>
         <NavLink to="/"><h1 className='font-bold text-2xl '>Imperio Yugioh</h1></NavLink>
@@ -22,15 +22,15 @@ const Navbar = () => {
 
         </div>
         <div>
-          <button className='login-btn'>Login <FiLogIn /></button>
+          
         </div>
       </div>
       
         
        
        
-      <ul className='flex flex-col md:flex-row md:items-center gap-6 text-lg font-medium  '>
-        
+      <ul className='flex  md:flex-row md:items-center gap-6 text-lg font-medium  '>
+        <li><button className=''>Login <FiLogIn /></button></li>
         <li className='hover:border-b-1'><NavLink to="/"><p>Home</p></NavLink></li>
         <li className='hover:border-b-1'><NavLink to="/producto">Productos</NavLink></li>
         <li className='hover:border-b-1'><NavLink to="/categoria">Categorias</NavLink></li>
