1734e3e0521698e91debf178c33418d71affb773
Lucas Sorzio <sorziolucas4@gmail.com>
2025-10-22 23:02:38 -0300

    m

--- src/Componentes/Home/Home.jsx
+++ src/Componentes/Home/Home.jsx
@@ -5,7 +5,7 @@ import LinkProduct from '../linkProduct/LinkProduct.jsx'
 
 const Home = () => {
   return (
-    <div className=' flex flex-col bg-gradient-to-b   from-blue-900 to-black min-h-screen   '>
+    <div className=' flex flex-col min-h-screen bg-gradient-to-b from-[#0b1444] to-[#081034] text-white"  '>
      
       
      <div className='flex-grow'>
