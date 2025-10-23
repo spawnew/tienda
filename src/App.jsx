0b436b9e9d657c72c1d3438c4e3f1428a1b4a232
Lucas Sorzio <sorziolucas4@gmail.com>
2025-10-22 23:30:09 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 import Contacto from './Componentes/Contacto/Contacto.jsx'
 import Footer from './Componentes/Footer/Footer.jsx'
@@ -10,30 +9,31 @@ import Categorias from './Componentes/Categorias/Categorias.jsx'
 import Home from './Componentes/Home/Home.jsx'
 import { CartProvider } from './Context/ContextCart.jsx'
 import Detalles from './Componentes/Productos/Detalles/Detalles.jsx'
+
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">  {/* Estructura base */}
-      <BrowserRouter>
-        <CartProvider>
+    <BrowserRouter>
+      <CartProvider>
+        {/* Contenedor general con altura completa */}
+        <div className="flex flex-col min-h-screen bg-gradient-to-b from-[#0b1444] to-[#081034]">
           <Navbar />
           
-          {/* Contenido principal */}
+          {/* Área del contenido que se expande */}
           <main className="flex-grow">
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/categoria" element={<Categorias />} />
-              <Route path="/producto" element={<Productos />} />
-              <Route path="/producto/detalles/:id" element={<Detalles />} />
-              <Route path="/contacto" element={<Contacto />} />
-              <Route path="/carrito" element={<Carrito />} />
+              <Route path='/' element={<Home />} />
+              <Route path='/categoria' element={<Categorias />} />
+              <Route path='/producto' element={<Productos />} />
+              <Route path='/producto/detalles/:id' element={<Detalles />} />
+              <Route path='/contacto' element={<Contacto />} />
+              <Route path='/carrito' element={<Carrito />} />
             </Routes>
           </main>
 
-          {/* Footer al final */}
           <Footer />
-        </CartProvider>
-      </BrowserRouter>
-    </div>
+        </div>
+      </CartProvider>
+    </BrowserRouter>
   )
 }
 
