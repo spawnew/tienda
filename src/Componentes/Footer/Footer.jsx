cd564ee70db0a10b760b3fb784d6fe30fd770dd9
Lucas Sorzio <sorziolucas4@gmail.com>
2025-10-22 23:16:48 -0300

    p

--- src/Componentes/Footer/Footer.jsx
+++ src/Componentes/Footer/Footer.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import './footer.css'
+
 const Footer = () => {
   return (
     
-      <div className="  bottom-0 left-0 w-full bg-gray-900 text-white text-center mt-6">
+      <div className="bg-[#081034] text-center text-gray-400 py-3 text-sm mt-auto">
      
 
        
@@ -14,9 +13,8 @@ const Footer = () => {
         
      
 
-      <div className="text-center text-sm text-gray-500  ">
         © {new Date().getFullYear()} Imperio Yugioh. Todos los derechos reservados.
-      </div>
+      
     </div>
   )
 }
