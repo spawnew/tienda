9aa78f5be6b11464afc0d1c9f5ca028ce83ed7c3
Lucas Sorzio <sorziolucas4@gmail.com>
2025-11-25 12:30:41 -0300

    L

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
 
           <Routes>
 
-            {/* Layout principal */}
+          
             <Route element={<MainLayout />}>
               <Route path='/' element={<Home />} />
               <Route path='/producto' element={<ItemListContainer />} />
