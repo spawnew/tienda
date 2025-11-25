0d5b4ccad81985466364ea4a0b1609d63cd05be3
Lucas Sorzio <sorziolucas4@gmail.com>
2025-11-25 16:03:00 -0300

    o

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom";
+
 import  Navbar  from "../Navbar/Navbar.jsx";
+import { Link } from "react-router-dom";
+
 
 
 export const Header = () => {
   return (
     <header>
       <Link to={"/"}>
-       
+      <img src="/images/logo.png" alt="logo-panaderia" />
+      <h1 className="logo-panaderia">Panaderia</h1>
       </Link>
       <Navbar />
     </header>
