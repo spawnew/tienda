557dbc276e36730a09b46e72eda3e9bdc79475f9
Lucas Sorzio <sorziolucas4@gmail.com>
2025-11-24 23:32:28 -0300

    d

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import { CartProvider } from './Context/ContextCart.jsx';
 import { AuthProvider } from './Context/AuthContext/AuthProvider.jsx';
-import { MainLayout } from './Layouts/MainLayout';
-import { AdminLayout } from './Layouts/AdminLayout';
-import Navbar from './Componentes/Navbar/Navbar.jsx';
+import { MainLayout } from './Layouts/MainLayout.jsx';
+import { AdminLayout } from './Layouts/AdminLayout.jsx';
+
 import Footer from './Componentes/Footer/Footer.jsx';
 import Home from './Componentes/Home/Home.jsx';
 import Contacto from './Componentes/Contacto/Contacto.jsx';
