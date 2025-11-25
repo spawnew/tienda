7fd01887422263237120002476673b890d45a9b6
Lucas Sorzio <sorziolucas4@gmail.com>
2025-11-24 23:15:29 -0300

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,63 @@
-
-import Contacto from './Componentes/Contacto/Contacto.jsx'
-import Footer from './Componentes/Footer/Footer.jsx'
-import Navbar from './Componentes/Navbar/Navbar.jsx'
-import { ItemListContainer }  from './Componentes/Productos/ItemListContainer'
-import Carrito from './Componentes/Carrito/Carrito.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import 'react-toastify/dist/ReactToastify.css';
-import Home from './Componentes/Home/Home.jsx'
-import { CartProvider } from './Context/ContextCart.jsx'
-import {ItemDetailContainer} from './Componentes/Productos/ItemDetailContainer/ItemDetailContainer.jsx'
-import './App.css'
-import { AuthProvider } from './Context/AuthContext/AuthProvider.jsx'
-import {Login} from './Componentes/Login/Login.jsx'
-import {RutaProtegida} from './Componentes/RutaProtegida/RutaProtegida'
-import { MainLayout } from './Layouts/MainLayout'
-import { AdminLayout } from './Layouts/AdminLayout'
-import { ProductFormContainer } from './Componentes/adminComponents/ProductFormContainer/ProductFormContainer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+import './App.css';
+
+// Contextos
+import { CartProvider } from './Context/ContextCart.jsx';
+import { AuthProvider } from './Context/AuthContext/AuthProvider.jsx';
+
+// Layouts
+import { MainLayout } from './Layouts/MainLayout.jsx';
+import { AdminLayout } from './Layouts/AdminLayout.jsx';
+
+// Componentes
+import Navbar from './Componentes/Navbar/Navbar.jsx';
+import Footer from './Componentes/Footer/Footer.jsx';
+import Home from './Componentes/Home/Home.jsx';
+import Contacto from './Componentes/Contacto/Contacto.jsx';
+import Carrito from './Componentes/Carrito/Carrito.jsx';
+import { ItemListContainer } from './Componentes/Productos/ItemListContainer';
+import { ItemDetailContainer } from './Componentes/Productos/ItemDetailContainer/ItemDetailContainer.jsx';
+import { Login } from './Componentes/Login/Login.jsx';
+import { RutaProtegida } from './Componentes/RutaProtegida/RutaProtegida';
+import { ProductFormContainer } from './Componentes/adminComponents/ProductFormContainer/ProductFormContainer';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
+        <CartProvider>
 
-  
-      <CartProvider>
-       
-       
-        
-          
-         
-        
           <Routes>
-             <Route element={<MainLayout/>}>
+
+            {/* Layout principal */}
+            <Route element={<MainLayout />}>
               <Route path='/' element={<Home />} />
-              <Route path='/producto/:category' element={<ItemListContainer />} />
               <Route path='/producto' element={<ItemListContainer />} />
-              <Route path='/detail/:id' element={<ItemDetailContainer/>} />
+              <Route path='/producto/:category' element={<ItemListContainer />} />
+              <Route path='/detail/:id' element={<ItemDetailContainer />} />
               <Route path='/contacto' element={<Contacto />} />
               <Route path='/carrito' element={<Carrito />} />
             </Route>
-            
-           <Route path="/admin" element={<AdminLayout/>}>
-                <Route index element={<Login />} />
-              
-                <Route 
-                  path="alta-productos"
-                  element={
-                    <RutaProtegida>
-                      <ProductFormContainer/>
-                    </RutaProtegida>
-                  } 
-                />
-              </Route>
-          </Routes >
+
+            {/* Layout admin */}
+            <Route path="/admin" element={<AdminLayout />}>
+              <Route index element={<Login />} />
+              <Route 
+                path="alta-productos"
+                element={
+                  <RutaProtegida>
+                    <ProductFormContainer />
+                  </RutaProtegida>
+                }
+              />
+            </Route>
+
+          </Routes>
+
+          {/* Toast */}
           <ToastContainer 
             position="top-right"
             autoClose={1000}
@@ -63,12 +67,13 @@ function App() {
             draggable
             theme="blue"
           />
+
           <Footer />
-     
-      </CartProvider>
-     </AuthProvider>
+
+        </CartProvider>
+      </AuthProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
