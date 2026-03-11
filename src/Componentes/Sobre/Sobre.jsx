fbd4b69a43c193f09ef5adab85164720eb4dfa48
Lucas Sorzio <sorziolucas4@gmail.com>
2026-03-11 15:39:50 -0300

    mejora de estilos

--- src/Componentes/Sobre/Sobre.jsx
+++ src/Componentes/Sobre/Sobre.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Sobre = () => {
   return (
-      <div data-aos="fade-up" className=' text-amber-50 w-80 h-fit lg:w-200 lg:flex-row flex-wrap flex flex-col md:flex-row'>
-          <div>
-    <h1 className='text-3xl font-bold text-amber-100 p-4'>Bienvenido a Imperio Yugioh</h1>
-    <p  className=' p-4 md:p-4 text-sm md:text-2xl font-serif '>Bienvenidos a nuestra tienda en línea, donde los duelistas y los coleccionistas se reunen.  . Gracias por elegirnos como su destino de compras confiable.</p>
-      </div>
+      <div data-aos="fade-up" className=' bg-gray-950 justify-content:center items-center text-amber-50 w-80 h-fit lg:w-200 lg:flex-row flex-wrap flex flex-col md:flex-row justify-center'>
+          
+    <h1 className='text-center text-3xl font-bold text-amber-100 p-4'>Bienvenido a Imperio Yugioh</h1>
+    
+    <p className='text-center text-amber-100 font-extralight text-lg p-4'>En Imperio Yugioh, somos apasionados por el mundo de las cartas coleccionables y nos dedicamos a ofrecerte la mejor experiencia de compra. Nuestra tienda en línea es el destino perfecto para los fanáticos de Yu-Gi-Oh! que buscan expandir su colección o encontrar esa carta especial que les falta.</p>
  </div>
  
   )
